6ae844d3ac5a2e85c97e7cf8675fa64bc49fbf5c
se-yoshi <se.yoshi@gmail.com>
2025-08-10 23:45:35 +0900

    first commit

---
+++ src/test/java/jp/gr/java_conf/stardiopside/jnotes/JnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.gr.java_conf.stardiopside.jnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
